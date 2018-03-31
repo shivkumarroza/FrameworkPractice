9089b8bbf3f911e5b8911f92541e408343caeb89
shivkumarroza <shivkumar.roja@gmail.com>
2018-03-31 20:22:26 +0530

    first commit

---
+++ src/script/ValidLogin.java
@@ -0,0 +1,27 @@
+package script;
+
+import org.testng.annotations.Test;
+
+import generic.BaseTest;
+import generic.Excel;
+import page.LoginPage;
+
+public class ValidLogin extends BaseTest {
+	
+	@Test
+	public void testValidLogin()
+	{
+		String loginPageTitle=Excel.getData(XL_PATH, "ValidLogin", 1, 0);
+		String un=Excel.getData(XL_PATH, "ValidLogin", 1, 1);
+		String pw=Excel.getData(XL_PATH, "ValidLogin", 1, 2);
+		String homePageTitle=Excel.getData(XL_PATH, "ValidLogin", 1, 3);
+		
+		LoginPage l=new LoginPage(driver);
+		l.verifyPageIsDisplayed(driver, loginPageTitle);
+		l.setUserName(un);
+		l.setPassword(pw);
+		l.clickLoginBTN();
+		l.verifyPageIsDisplayed(driver, homePageTitle);
+	}
+
+}
